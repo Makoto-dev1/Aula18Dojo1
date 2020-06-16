1ca7dc2d687a44d9fc5beab9a0fa8a31a39f4a88
Makoto-dev1 <61596227+Makoto-dev1@users.noreply.github.com>
2020-06-16 15:06:50 -0300

    a

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula18Dojo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Jogador messi = new Jogador();
+            messi.Nome = "Messi";
+            messi.Posicao = "Atacante";
+            messi.Nascimento = DateTime.Parse("24/06/1987");            
+
+            messi.MostrarDados();
+            Console.WriteLine( messi.CalcularIdade() );
+            Console.WriteLine( messi.CalcularAposentadoria() );
+        }
+    }
+}
